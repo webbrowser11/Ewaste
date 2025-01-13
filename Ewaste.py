fcd3fac6a229367a78d1c179e90fe8d3416a81b1
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-12 18:11:57 -0800

    1.1

--- Ewaste.py
+++ Ewaste.py
@@ -1,4 +1,4 @@
-# Ewaste 1.0, copyright 2025 webbrowser11
+# Ewaste 1.1, copyright 2025 webbrowser11
 import os
 import sys
 import shutil 
@@ -12,10 +12,13 @@ if "Ewaste" in FolderPath:
     DelConfirm = input(f"You are about to remove these files: {Contents} Are you sure [Y/N]: ")
     
     if DelConfirm == "y":
-        shutil.rmtree(FolderPath)
-        print("Deleted your Ewaste. Exiting soon please do not close.")
-        time.sleep(3)
-        sys.exit()
+        try:
+            shutil.rmtree(FolderPath)
+            print("Deleted your Ewaste. Exiting soon please do not close.")
+            time.sleep(3)
+            sys.exit()
+        except Exception as error:
+            print(f"A unexpected error occured: {error}")
     else:
         print("Okay. Exiting soon please do not close.")
         time.sleep(3)
